0415684ca3ae62528bbf66106be103107e69f680
Hana Klingová <hana.klingova@gmail.com>
2018-03-14 16:47:34 +0100

    implement function for random computer choice

--- js/game.js
+++ js/game.js
@@ -1,3 +1,15 @@
 (function() {
   "use strict";
+
+  function computerPlay() {
+    let randInt = Math.floor(Math.random() * 3);
+    if (randInt === 1) {
+      return "rock";
+    } else if (randInt === 2) {
+      return "paper";
+    } else {
+      return "scissors";
+    }
+  }
+
 }());
