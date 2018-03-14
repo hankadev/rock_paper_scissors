630b5b3a03e7867692df3bacef25e2904899d424
Hana Klingová <hana.klingova@gmail.com>
2018-03-14 17:23:26 +0100

    add playRound function to decide a winner of game

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
   "use strict";
 
   function computerPlay() {
-    let randInt = Math.floor(Math.random() * 3);
+    const randInt = Math.floor(Math.random() * 3);
     if (randInt === 1) {
       return "rock";
     } else if (randInt === 2) {
@@ -11,5 +11,33 @@
       return "scissors";
     }
   }
+
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      return "Tie!"
+    } else if (playerSelection === "rock") {
+      if (computerSelection === "paper") {
+        return "You lost! Paper beats rock."
+      } else {
+        return "You won! Rock beats scissors."
+      }
+    } else if (playerSelection === "paper") {
+      if (computerSelection === "rock") {
+        return "You won! Paper beats rock."
+      } else {
+        return "You lost! Scissors beats paper."
+      }
+    } else {
+      if (computerSelection === "rock") {
+        return "You lost! Rock beats scissors."
+      } else {
+        return "You won! Scissors beats paper."
+      }
+    }
+  }
+
+  const playerSelection = "scissors";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
 
 }());
